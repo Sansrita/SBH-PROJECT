4075bfcef801ac790330e025b71be508b1564f91
Sansrita <sansritasaha@gmail.com>
2023-04-11 00:01:24 +0530

    first commit

---
+++ src/components/about.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export const About = (props) => {
+  return (
+    <div id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            {" "}
+            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+          </div>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
+              <h2>Patient Details</h2>
+              <p>Name: Debopom Banerjee</p>
+              <p>Age: 19</p>
+              <p>Sex: Male</p>
+              <p>Weight: 50</p>
+              <p>Phone Number: 123456</p>
+              <p>Address: Durgapur </p>
+              <p>Mailid (if any ): </p>
+              <h3>Patient's Problems:</h3>
+              <div className="list-style">
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why.map((d, i) => (
+                          <li key={`${d}-${i}`}>headache</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why2.map((d, i) => (
+                          <li key={`${d}-${i}`}> fever</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const Header = (props) => {
+  return (
+    <header id="header">
+      <div className="intro">
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  Welcome to our Site!
+                  <span></span>
+                </h1>
+                <p><b>This is a realtime hospitral management system with Face recognisation</b></p>
+                <a
+                  href="#features"
+                  className="btn btn-custom btn-lg page-scroll"
+                >
+                  Learn More
+                </a>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
